4d733684c39221809450d50b4706ea727fff6ab8
Andrea <andrea@andreas-mini.home>
2017-04-23 21:25:19 +0100

    added express module

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(req, res){
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	var readHtml = fs.createReadStream(__dirname + '/index.html', 'utf8');
-	readHtml.pipe(res);
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
 });
 
-server.listen(3000, '127.0.0.1');
+app.use(express.static('./public'));
+app.listen(3000);
 console.log('listening on port 3000');
