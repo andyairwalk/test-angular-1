a6689611a1174a17edeeda89b85a57d7174032fd
Andrea <andrea@andreas-mini.home>
2017-04-26 21:11:49 +0100

    created first simple controller

--- public/js/my-angular-app.js
+++ public/js/my-angular-app.js
@@ -3,3 +3,11 @@ var myFirstApp = angular.module("myFirstApp", []);
 myFirstApp.config(function(){
 	console.log('so far so good');
 });
+
+myFirstApp.run(function(){
+
+});
+
+myFirstApp.controller('MyController1', function($scope){
+	$scope.message = 'hi everyone!';
+});
